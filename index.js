10c8a9f244bee597bbbc6f0cf2fbc889d139f477
StasTerletskiy <stanislavterletskiy1@gmail.com>
2020-06-29 16:12:14 +0300

    Second Math.random

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 // Выведите на экран случайное целое число от 1 до 100.
 // Заполните массив 10-ю случайными целыми числами. (Подсказка: нужно воспользоваться циклами for или while).
 
-alert(Math.round(Math.random()*100));
+const res = [];
+const DELIMITER = 10;
+
+for(let i = 0; i < DELIMITER; ++i)
+{
+  res.push(Math.round(Math.random()*10));
+}
+
+alert(res);
